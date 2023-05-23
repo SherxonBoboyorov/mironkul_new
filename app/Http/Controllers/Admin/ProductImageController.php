29f36d5873f2c61113febc4c2e4f0a1eb86c4782
SherxonBoboyorov <sherxonboboyorov@gmail.com>
2023-05-23 18:23:00 +0500

    changes

--- app/Http/Controllers/Admin/ProductImageController.php
+++ app/Http/Controllers/Admin/ProductImageController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\CreateProductImage;
+use App\Http\Requests\Admin\UpdateProductImage;
+use App\Models\Product;
+use App\Models\ProductImage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class ProductImageController extends Controller
 {
@@ -14,7 +19,10 @@ class ProductImageController extends Controller
      */
     public function index()
     {
-        //
+        $productimages = ProductImage::orderBy('id')->paginate(12);
+        return view('admin.productimage.index', [
+            'productimages' => $productimages
+        ]);
     }
 
     /**
@@ -24,18 +32,25 @@ class ProductImageController extends Controller
      */
     public function create()
     {
-        //
+        $prodcuts = Product::all();
+        return view('admin.productimage.create', compact('prodcuts'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  App\Http\Requests\Admin\CreateProductImage  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateProductImage $request)
     {
-        //
+        $data = $request->all();
+        $data['image'] = ProductImage::uploadImage($request);
+
+        if (ProductImage::create($data)) {
+            return redirect()->route('productimage.index')->with('message', "Product Image created seccessfully");
+        }
+        return redirect()->route('productimage.index')->with('message', "unable to created Product Image");
     }
 
     /**
@@ -55,19 +70,23 @@ class ProductImageController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(ProductImage $productimage)
     {
-        //
+        $prodcut = Product::all();
+        return view('admin.productimage.edit', [
+            'product' => $prodcut,
+            'productimage' => $productimage
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  App\Http\Requests\Admin\UpdateProductImage  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateProductImage $request, $id)
     {
         //
     }

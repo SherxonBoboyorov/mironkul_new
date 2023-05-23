2b8453e92102ef956e008100e9245615d282d3cc
SherxonBoboyorov <sherxonboboyorov@gmail.com>
2023-05-23 16:46:48 +0500

    changes

--- database/migrations/2023_05_23_103803_create_products_table.php
+++ database/migrations/2023_05_23_103803_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-
+            
             $table->string('image');
 
             $table->string('title_ru');

194b970dc5e262788b7589eb9aa11d1098b7be61
Sherxon <116145014+SherxonBoboyorov@users.noreply.github.com>
2023-07-13 17:39:36 +0500

    last changes

--- public/front/js/loading.js
+++ public/front/js/loading.js
@@ -1,19 +1,3 @@
-const headerI = document.querySelector('.header')
-if (headerI) {
-  const icons1 = document.querySelector('.icons1')
-  const icons2 = document.querySelector('.icons2')
-  icons1.style.display = 'none'
-  icons2.style.display = 'none'
-  setTimeout(() => {
-    icons1.style.display = 'block'
-  }, 400)
-
-  setTimeout(() => {
-    icons2.style.display = 'block'
-  }, 700)
-
-  window.addEventListener('DOMContentLoaded', () => {
-    $('.lds-roller').fadeOut()
-    $('#loading').delay(4000).fadeOut('slow')
-  })
-}
+window.addEventListener('DOMContentLoaded', () => {
+  $('#loading').delay(1000).fadeOut(0)
+})
--- public/front/js/slic.js
+++ public/front/js/slic.js
@@ -1,119 +1,39 @@
-const slider = document.querySelector('.slider')
-if (slider) {
-  const slides = document.querySelectorAll('.slide')
-  const controls = document.querySelectorAll('.slider__button')
-  const menuList = document.querySelectorAll('.slider__list__cart')
-  const activeMenu = document.querySelector('.slider__menu__cart')
-  let activeSlide = 0
-  let prevActive = 0
-
-  let int = setInterval(changeSlides, 60000)
-  let max = setInterval(listActive, 6000)
-
-  function changeSlides() {
-    slides[prevActive].classList.remove('active')
-    controls[prevActive].classList.remove('active')
-    menuList[prevActive].classList.remove('active')
-
-    slides[activeSlide].classList.add('active')
-    controls[activeSlide].classList.add('active')
-    menuList[activeSlide].classList.add('active')
-
-    prevActive = activeSlide++
-
-    if (activeSlide >= slides.length) {
-      activeSlide = 0
-    }
-  }
-
-  function listActive() {
-    const item = activeMenu.querySelectorAll('.slider__list__cart')
-    for (let i = 0; i < item.length; i++) {
-      if (item[i].className == 'slider__list__cart active') {
-        const itemIn = item[i].querySelectorAll('li')
-
-        // item style start
-
-        var counter = 0
-        const media1 = window.matchMedia('(min-width: 1280px)')
-        const media2 = window.matchMedia('(min-width: 1000px)')
-        const media3 = window.matchMedia('(min-width: 600px)')
-        const media4 = window.matchMedia('(min-width: 350px)')
-        const media5 = window.matchMedia('(min-width: 200px)')
-        if (media1.matches) {
-          if (itemIn.length <= 5) {
-            counter = itemIn.length
-          } else {
-            counter = 5
-          }
-          item[i].style.gridTemplateColumns = `repeat(${counter},1fr)`
-        } else if (media2.matches) {
-          if (itemIn.length <= 4) {
-            counter = itemIn.length
-          } else {
-            counter = 4
-          }
-          item[i].style.gridTemplateColumns = `repeat(${counter},1fr)`
-        } else if (media3.matches) {
-          if (itemIn.length <= 3) {
-            counter = itemIn.length
-          } else {
-            counter = 3
-          }
-          item[i].style.gridTemplateColumns = `repeat(${counter},1fr)`
-        } else if (media4.matches) {
-          if (itemIn.length <= 2) {
-            counter = itemIn.length
-          } else {
-            counter = 2
-          }
-          item[i].style.gridTemplateColumns = `repeat(${counter},1fr)`
-        } else if (media5.matches) {
-          if (itemIn.length <= 1) {
-            counter = itemIn.length
-          } else {
-            counter = 1
-          }
-          item[i].style.gridTemplateColumns = `repeat(${counter},1fr)`
-        }
-        // item style end
-
-        for (const i of itemIn) {
-          i.classList.add('fadeInDown')
-        }
-        itemIn[0].classList.add('fadeInLeft')
-        itemIn[itemIn.length - 1].classList.add('fadeInRight')
-      } else {
-        const itemIn = item[i].querySelectorAll('li')
-        for (const i of itemIn) {
-          i.classList.add('animated')
-          i.classList.remove('fadeInDown')
-        }
-        itemIn[0].classList.remove('fadeInLeft')
-        itemIn[itemIn.length - 1].classList.remove('fadeInRight')
-
-        itemIn[0].classList.add('animated')
-        itemIn[itemIn.length - 1].classList.add('animated')
-      }
-    }
-  }
-
-  controls.forEach((control) => {
-    control.addEventListener('click', () => {
-      let idx = [...controls].findIndex((c) => c === control)
-      activeSlide = idx
-
-      changeSlides()
-      listActive()
-
-      clearInterval(int, max)
-      int = setInterval(changeSlides, 60000)
-      max = setInterval(listActive, 60000)
-    })
-  })
-}
-
 $(document).ready(function () {
+  $('.slider__menu').slick({
+    arrows: false,
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1399.9,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 1120,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 776,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  })
+
   $('.fotogalereya_in__list1').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -175,4 +95,31 @@ $(document).ready(function () {
       },
     ],
   })
+
+  const slider = document.querySelector('.slider')
+  let linkTemp
+  if (slider) {
+    const menuItems = document.querySelectorAll('.slider__menu__item')
+    const sliderBgs = document.querySelectorAll('.slider__bg__item')
+
+    const sliderLinks = document.querySelectorAll('.slider__link__list>li')
+
+    menuItems.forEach((item, menuItemIndex) => {
+      item.addEventListener('mouseover', () => {
+        if (sliderLinks.length === 2 && linkTemp !== menuItemIndex) {
+          const productsLink = sliderLinks[0].querySelector('a')
+          const portfolioLink = sliderLinks[1].querySelector('a')
+          productsLink.href = productsLink.href.replace(/\/([^/]+)$/, `/products${menuItemIndex === 0 ? '' : menuItemIndex}`)
+          portfolioLink.href = portfolioLink.href.replace(/\/([^/]+)$/, `/portfolios${menuItemIndex === 0 ? '' : menuItemIndex}`)
+
+          linkTemp = menuItemIndex
+        }
+
+        sliderBgs.forEach((sliderBg, sliderBgIndex) => {
+          if (menuItemIndex !== sliderBgIndex) sliderBg.classList.remove('active')
+          else sliderBg.classList.add('active')
+        })
+      })
+    })
+  }
 })
